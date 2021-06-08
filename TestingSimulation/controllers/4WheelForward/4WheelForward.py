2fd632a4a115e0c4c9bc223b0e30172abf7231b2
Estox <kacp.kuj@gmail.com>
2021-06-08 14:38:02 +0200

    Robot-in-robot simulation

---
+++ TestingSimulation/controllers/4WheelForward/4WheelForward.py
@@ -0,0 +1,35 @@
+"""4WheelForward controller."""
+
+# You may need to import some classes of the controller module. Ex:
+#  from controller import Robot, Motor, DistanceSensor
+from controller import Robot
+
+# create the Robot instance.
+robot = Robot()
+
+# get the time step of the current world.
+timestep = int(robot.getBasicTimeStep())
+
+# You should insert a getDevice-like function in order to get the
+# instance of a device of the robot. Something like:
+#  motor = robot.getDevice('motorname')
+#  ds = robot.getDevice('dsname')
+#  ds.enable(timestep)
+wheels = []
+wheelsNames = ['wheel1', 'wheel2', 'wheel3', 'wheel4']
+for i in range(4):
+    wheels.append(robot.getDevice(wheelsNames[i]))
+    wheels[i].setPosition(float('inf'))
+    wheels[i].setVelocity(0.0)
+# Main loop:
+# - perform simulation steps until Webots is stopping the controller
+while robot.step(timestep) != -1:
+    leftSpeed = 1.0
+    rightSpeed = 1.0
+    wheels[0].setVelocity(leftSpeed)
+    wheels[1].setVelocity(rightSpeed)
+    wheels[2].setVelocity(leftSpeed)
+    wheels[3].setVelocity(rightSpeed)
+
+
+# Enter here exit cleanup code.
