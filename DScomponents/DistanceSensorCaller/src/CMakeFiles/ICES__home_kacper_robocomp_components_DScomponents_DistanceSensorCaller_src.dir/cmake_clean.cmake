591fc71a16fb5e38ac630005cf8ada4a381c79ea
Estox <kacp.kuj@gmail.com>
2021-06-17 19:41:51 +0200

    WIP: Distance components

---
+++ DScomponents/DistanceSensorCaller/src/CMakeFiles/ICES__home_kacper_robocomp_components_DScomponents_DistanceSensorCaller_src.dir/cmake_clean.cmake
@@ -0,0 +1,5 @@
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/ICES__home_kacper_robocomp_components_DScomponents_DistanceSensorCaller_src.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
