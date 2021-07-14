f48cd1127083c2d2b972da0005b06c592a2b8672
Estox <kacp.kuj@gmail.com>
2021-07-14 16:09:41 +0200

    Delete CMake directories.

--- TestingCommunication/Caller/src/CMakeFiles/ICES__home_kacper_robocomp_components_TestingCommunication_Caller_src.dir/cmake_clean.cmake
+++
@@ -1,5 +0,0 @@
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/ICES__home_kacper_robocomp_components_TestingCommunication_Caller_src.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
